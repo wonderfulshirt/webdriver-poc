02ba3468fb9a11fccd5547880206714b44559100
Andrew Slavin <andrew.slavin@engineering.digital.dwp.gov.uk>
2020-03-11 11:43:52 +0000

    Remove unused imports

--- src/test/java/com/wonderfulshirt/pageobjects/BasePage.java
+++ src/test/java/com/wonderfulshirt/pageobjects/BasePage.java
@@ -1,10 +1,8 @@
 package com.wonderfulshirt.pageobjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
